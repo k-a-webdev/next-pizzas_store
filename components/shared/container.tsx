2854a1ba8e14237b51d2b4ea8d69102771e097ba
k-a-webdev <kuzko.andrii.webdev@gmail.com>
2024-08-01 16:21:08 +0400

    feat: add additional components
    Signed-off-by: k-a-webdev <kuzko.andrii.webdev@gmail.com>

---
+++ components/shared/container.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { cn } from "/lib/utils";
+
+interface containerProps {
+  className?: string;
+}
+
+export const Container: React.FC<React.PropsWithChildren<containerProps>> = ({
+  className,
+  children,
+}) => {
+  return <div className={cn("mx-auto max-w-[1280px]", className)}>{children}</div>;
+};
+
+export default Container;
