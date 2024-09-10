9ccc038d9533c004bb9b799e34ea87180cfd4969
k-a-webdev <kuzko.andrii.webdev@gmail.com>
2024-09-10 20:47:45 +0400

    feat: implement Zustand and Prisma with postgresql in Vercel
    Signed-off-by: k-a-webdev <kuzko.andrii.webdev@gmail.com>

--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -3,29 +3,39 @@
 import React from "react";
 
 import { cn } from "/lib/utils";
+import { useCategoryStore } from "/store/category";
 
 interface categoriesProps {
   className?: string;
 }
 
-const categoryList = ["Піца", "Комбо", "Закуски", "Коктейлі", "Кава", "Напої", "Десерти"];
-const activeCategory = 0;
+const categoryList = [
+  { id: 1, name: "Піца" },
+  { id: 2, name: "Комбо" },
+  { id: 3, name: "Закуски" },
+  { id: 4, name: "Коктейлі" },
+  { id: 5, name: "Кава" },
+  { id: 6, name: "Напої" },
+  { id: 7, name: "Десерти" },
+];
 
 export const Categories: React.FC<categoriesProps> = ({ className }) => {
-  const [active, setActive] = React.useState(0);
+  const { activeId, setActiveId } = useCategoryStore((state) => state);
+  console.log(activeId);
 
   return (
     <div className={cn("inline-flex gap-1 bg-gray-50 p-1 rounded-2xl", className)}>
-      {categoryList.map((el, index) => {
+      {categoryList.map(({ id, name }, index) => {
         return (
           <a
             className={cn(
               "flex items-center font-bold h-11 rounded-2xl px-5",
-              active === index && "bg-white shadow-md shadow-gray-200 text-primary"
+              activeId === id && "bg-white shadow-md shadow-gray-200 text-primary"
             )}
-            onClick={() => setActive(index)}
+            onClick={() => setActiveId(id)}
+            href={`/#${name}`}
             key={index}>
-            <button>{el}</button>
+            <button>{name}</button>
           </a>
         );
       })}
--- components/shared/productGroupList.tsx
+++ components/shared/productGroupList.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import React from "react";
+import { useIntersection } from "react-use";
 
 import { cn } from "/lib/utils";
 import { ProductCard, Title } from "./index";
+import { useCategoryStore } from "/store/category";
 
 interface productGroupListProps {
   title: string;
@@ -18,17 +22,27 @@ export const ProductGroupList: React.FC<productGroupListProps> = ({
   className,
   listClassName,
 }) => {
+  const setActiveCategoryId = useCategoryStore((state) => state.setActiveId);
+  const intersectionRef = React.useRef(null);
+  const intersection = useIntersection(intersectionRef, { threshold: 0.4 });
+
+  React.useEffect(() => {
+    if (intersection?.isIntersecting) {
+      setActiveCategoryId(categoryId);
+    }
+  }, [categoryId, intersection?.isIntersecting]);
+
   return (
-    <div className={cn("", className)}>
+    <div className={cn("", className)} id={title} ref={intersectionRef}>
       <Title size="lg" className="font-extrabold mb-5">
         {title}
       </Title>
 
       <div className={cn("grid grid-cols-3 gap-[50px]", listClassName)}>
-        {items.map((item, index) => {
+        {items.map((item) => {
           return (
             <ProductCard
-              key={item.id}
+              key={categoryId}
               id={item.id}
               name={item.name}
               imageUrl={item.imageUrl}
